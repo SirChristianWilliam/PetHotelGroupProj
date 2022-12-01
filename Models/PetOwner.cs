f2bcf79e5948a92ff41d30a220ba533783961493
Chris Maki <c.william.developer@gmail.com>
2022-12-01 14:02:15 -0600

    PetOwners DB set up, PetOwners table created

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,17 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner 
+    {
+        public int id {get; set;}
+
+        [Required]
+        public string name  {get; set;}
+
+        [Required]
+        public string email {get; set;}
+
+        [Required]
+        public int pets {get; set;}
+    }
 }
